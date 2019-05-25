43d10cdd20fc5b4bb7c5366a8092f86b7b986bb3
pranav <pranavverma550@gmail.com>
2019-05-25 08:15:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'InputOutput1';
+money= "100000000";
+property="house";
+public CData: number;
 }
